b322fdeff1b43b3e989fd66407c55617f76e6604
EliasNybye <eliasnybye@gmail.com>
2026-01-07 17:52:10 +0100

    Delete old file

--- character input datetime.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-current_year = datetime.datetime.now().year
-name = input ("whats is your name? ")
-age = int(input("How old are you? "))
-print(f"{name}, you will be 100 years old in {current_year - age + 100}")
